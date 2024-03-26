38abf16324e16b2657d1cd3cd18ec35b987773c9
deltaDC <ducchinh.work@gmail.com>
2024-03-26 14:35:56 +0700

    add json ignore

--- src/main/java/com/deltadc/examsystem/Exam/Exam.java
+++ src/main/java/com/deltadc/examsystem/Exam/Exam.java
@@ -6,6 +6,7 @@ import com.deltadc.examsystem.ExamStatistic.ExamStatistic;
 import com.deltadc.examsystem.Question.Question;
 import com.deltadc.examsystem.UserAnswer.UserAnswer;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,17 +43,22 @@ public class Exam {
     private Date endTime;
 
     @OneToMany(mappedBy = "exam")
+    @JsonIgnore
     private List<ExamResult> examResults;
 
     @OneToMany(mappedBy = "exam")
+    @JsonIgnore
     private List<ExamAttempt> examAttempts;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "exam")
     private List<Question> questions;
 
+    @JsonIgnore
     @OneToOne(mappedBy = "exam")
     private ExamStatistic examStatistic;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "exam")
     private List<UserAnswer> userAnswers;
 }
--- src/main/java/com/deltadc/examsystem/ExamResult/ExamResult.java
+++ src/main/java/com/deltadc/examsystem/ExamResult/ExamResult.java
@@ -2,6 +2,7 @@ package com.deltadc.examsystem.ExamResult;
 
 import com.deltadc.examsystem.Exam.Exam;
 import com.deltadc.examsystem.User.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,10 +26,23 @@ public class ExamResult {
     private String status;
 
     @ManyToOne
-    @JoinColumn(name = "userId")
+    @JsonIgnore
+    @JoinColumn(name = "userId", insertable = false, updatable=false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "examId")
+    @JsonIgnore
+    @JoinColumn(name = "examId", insertable = false, updatable=false)
     private Exam exam;
+
+    private Long userId;
+
+    private Long examId;
+
+    public ExamResult(Long userId, Long examId, int score, String status) {
+        this.score = score;
+        this.status = status;
+        this.userId = userId;
+        this.examId = examId;
+    }
 }
