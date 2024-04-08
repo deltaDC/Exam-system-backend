410a0c25024c87abc6114dd785b02b9d8e9988dd
deltaDC <ducchinh.work@gmail.com>
2024-04-08 21:42:50 +0700

    add user dto, fix password response in get users api

---
+++ src/main/java/com/deltadc/examsystem/User/UserDTO.java
@@ -0,0 +1,14 @@
+package com.deltadc.examsystem.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private Long userId;
+    private String username;
+    private String role;
+
+}
